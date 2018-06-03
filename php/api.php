837ab88b044fd2b7034b061b9fce211fc7563f85
Jianfeng Chen <352871242@qq.com>
2018-06-03 20:28:35 +0800

    Update api.php

--- php/api.php
+++ php/api.php
@@ -1,7 +1,7 @@
 <?php
 $dbconf=array("host" => "localhost",
             "user" => "root",
-            "pass" => "159357cjf",
+            "pass" => "youpass",
             "dbname"=>"webim");
 $s_conn=null;
 function mydie($status,$data){
